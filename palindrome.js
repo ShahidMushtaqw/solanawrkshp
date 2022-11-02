3f79fd9b23478cd1004c81273eab72ee4b32e1c9
Shahid Mushtaq <shahid.mushtaq.w@gmail.com>
2022-11-02 06:29:02 +0000

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,26 +1,12 @@
-const readline = require("readline");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-let userInput = "";
-rl.question("Enter the string\n", function(string) {
-  let f =1;
-  let n =string.length;
-for(let i =0,j=n-1;i<j;i++,j--){
-  
-        if(string[i]===string[j])
-        continue;
-        else{
-            f=0;
-            break;
-    } 
+function isPallindrome(s) {
+  let start = 0,
+    end = s.length - 1;
+  while (start < end) {
+    if (s[start++] != s[end--]) return false;
+  }
+  return true;
 }
-if(f===1)
-console.log("True");
-else
-console.log("False")
-  rl.close();
-});
+const s = process.argv[2];
+if (s) console.log(isPallindrome(s));
+else console.log("Enter string");
